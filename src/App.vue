b3811dce2e254b477fb6514746d2afc77bf733f2
TravkinEvgeny <asassinev@yandex.ru>
2021-01-22 15:49:45 +0300

    updated toolbar

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,76 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <v-app>
+    <v-navigation-drawer app temporary v-model="drawer">
+      <v-list
+        nav
+        dense>
+        <v-list-item-group
+          v-model="selectedItem"
+          color="primary">
+          <v-list-item
+            v-for="(link, i) in links"
+            :key="i"
+            :to="link.url">
+            <v-list-item-icon>
+              <v-icon v-text="link.icon"></v-icon>
+            </v-list-item-icon>
+            <v-list-item-content>
+              <v-list-item-title v-text="link.title"></v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list-item-group>
+      </v-list>
+    </v-navigation-drawer>
+    <v-card
+    color="grey lighten-4"
+    flat
+    tile
+    >
+      <v-toolbar dense dark color="primary ">
+        <v-app-bar-nav-icon
+          @click="drawer = !drawer"
+          class="d-none d-sm-flex d-md-none"
+        ></v-app-bar-nav-icon>
+        <v-toolbar-title>Ad applicatioin</v-toolbar-title>
+        <v-spacer></v-spacer>
+        <v-btn
+          class="d-sm-none d-md-flex"
+          plain
+          v-for="link in links"
+          :key="link.title"
+          :to="link.url"
+        >
+          <v-icon left>
+            {{ link.icon }}
+          </v-icon>
+          {{ link.title }}
+        </v-btn>
+      </v-toolbar>
+    </v-card>
+    <v-main>
+      <router-view></router-view>
+    </v-main>
+    <v-footer app></v-footer>
+  </v-app>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+<script>
+
+export default {
+  name: 'App',
+  data () {
+    return {
+      drawer: false,
+      selectedItem: 0,
+      links: [
+        { title: 'Login', icon: 'mdi-lock', url: '/login' },
+        { title: 'Registration', icon: 'mdi-face', url: '/registration' },
+        { title: 'Orders', icon: 'mdi-bookmark-outline', url: '/orders' },
+        { title: 'New ad', icon: 'mdi-note-plus', url: '/new' },
+        { title: 'My ads', icon: 'mdi-format-list-bulleted', url: '/list' }
+      ]
+    }
+  }
 }
 
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+</script>
