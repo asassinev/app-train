1eba06929e600ee46925ada2252cd4f32679fb1b
TravkinEvgeny <asassinev@yandex.ru>
2021-01-22 17:53:01 +0300

    created-login-page

--- src/components/Auth/Login.vue
+++ src/components/Auth/Login.vue
@@ -1,18 +1,92 @@
 <template>
-    <v-container>
-        <v-layout row>
-            <v-flex xs12>
-                <h1>Login</h1>
-            </v-flex>
-        </v-layout>
-    </v-container>
+  <v-container fluid fill-height>
+      <v-layout align-center justify-center>
+          <v-flex xs12 sm8 md4>
+            <v-card class="elevation-12">
+              <v-toolbar dark color="primary">
+                <v-toolbar-title>
+                  Login form
+                </v-toolbar-title>
+                <v-spacer></v-spacer>
+              </v-toolbar>
+              <v-card-text>
+                <v-form>
+                  <v-text-field
+                    type="email"
+                    prepend-icon="mdi-account"
+                    v-model="email"
+                    :error-messages="emailErrors"
+                    label="E-mail"
+                    required
+                    @input="$v.email.$touch()"
+                    @blur="$v.email.$touch()"
+                  ></v-text-field>
+                  <v-text-field
+                    prepend-icon="mdi-lock"
+                    name="password"
+                    label="Password"
+                    id="password"
+                    type="password"
+                    v-model="password"
+                    required
+                    @input="$v.password.$touch()"
+                    @blur="$v.password.$touch()"
+                    :error-messages="passwordErrors"
+                  ></v-text-field>
+                </v-form>
+              </v-card-text>
+              <v-card-actions>
+                <v-spacer></v-spacer>
+                <v-btn
+                  color="primary"
+                  @click="onSubmit"
+                >login</v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-flex>
+      </v-layout>
+  </v-container>
 </template>
 
 <script>
+import { validationMixin } from 'vuelidate'
+import { required, minLength, email } from 'vuelidate/lib/validators'
+
 export default {
+  mixins: [validationMixin],
+  validations: {
+    email: { required, email },
+    password: { required, minLength: minLength(6) }
+  },
   data () {
     return {
-
+      email: '',
+      password: '',
+      counter: ''
+    }
+  },
+  computed: {
+    passwordErrors () {
+      const errors = []
+      if (!this.$v.password.$dirty) return errors
+      !this.$v.password.minLength && errors.push('Min length length 6 characters.')
+      !this.$v.password.required && errors.push('Password is required.')
+      return errors
+    },
+    emailErrors () {
+      const errors = []
+      if (!this.$v.email.$dirty) return errors
+      !this.$v.email.email && errors.push('Must be valid e-mail')
+      !this.$v.email.required && errors.push('E-mail is required')
+      return errors
+    }
+  },
+  methods: {
+    onSubmit () {
+      this.$v.$touch()
+      if (!this.$v.$invalid) {
+        console.log({ email: this.email, password: this.password })
+      }
     }
   }
 }
--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
 import Vue from 'vue'
-import App from './App.vue'
 import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify'
+import Vuelidate from 'vuelidate'
+import App from './App.vue'
+
+Vue.use(Vuelidate)
 
 Vue.config.productionTip = false
 
