8dda7eb8e648b1776d5f0aa234c713265426e376
TravkinEvgeny <asassinev@yandex.ru>
2021-01-25 17:54:20 +0300

    added firebase

--- src/main.js
+++ src/main.js
@@ -3,6 +3,8 @@ import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify'
 import Vuelidate from 'vuelidate'
+import firebase from 'firebase'
+
 import App from './App.vue'
 
 Vue.use(Vuelidate)
@@ -13,5 +15,16 @@ new Vue({
   router,
   store,
   vuetify,
-  render: h => h(App)
+  render: h => h(App),
+  created () {
+    firebase.initializeApp({
+      apiKey: 'AIzaSyAUQpr-MpswzQEE9JjYLbJhzKf88Lyv2o4',
+      authDomain: 'app-ads-3671b.firebaseapp.com',
+      projectId: 'app-ads-3671b',
+      storageBucket: 'app-ads-3671b.appspot.com',
+      messagingSenderId: '1031949799924',
+      appId: '1:1031949799924:web:bad6895a339c8835d229a1',
+      measurementId: 'G-K9SV9VSX3'
+    })
+  }
 }).$mount('#app')
